89504461c602de356db796efc5e96870536c5313
Alexandre <alexandregaillard@me.com>
2026-05-25 19:17:25 +0200

    ajout tmdb id dans genre

--- models/genres.js
+++ models/genres.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const genresSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true}
+    name: {type: String, required: true, unique: true},
+    tmbdb_genre_id: {type: Number, required: true, unique: true}
 });
 
 const Genres = mongoose.model('genres', genresSchema);
