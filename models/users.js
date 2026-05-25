2e2433dc0946ae82434fc30093801e3bbf9097d3
Brice WeKeys <formation@wekeys.fr>
2026-05-25 19:42:45 +0200

    Correction connexion MongoDB

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const usersSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true},
   password: {type: String, required: true, unique: false},
   email: {type: String, required: true, unique: true},
@@ -27,6 +27,6 @@ const userSchema = new mongoose.Schema({
   }],
 });
 
-const User = mongoose.model('users', userSchema);
+const Users = mongoose.model('users', userSchema);
 
 module.exports = User;
