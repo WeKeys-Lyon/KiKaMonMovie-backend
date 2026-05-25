92d7404c51cf4e1de5a08e221d2e7fbba0d6946d
Alexandre <alexandregaillard@me.com>
2026-05-25 19:16:45 +0200

    ajout tmdb id dans composer directors et cast

--- models/directors.js
+++ models/directors.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const directorsSchema = new mongoose.Schema({
     name : {type: String, required: true, unique: false},
+    tmbdb_director_id: {type: Number, required: true, unique: true}
 });
 
 const Directors = mongoose.model('directors', directorsSchema);
