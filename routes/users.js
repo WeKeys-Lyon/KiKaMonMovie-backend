920e4cc63d330ed35019ea76c41ca036d089a47e
Alexandre <alexandregaillard@me.com>
2026-05-25 20:12:54 +0200

    modif check body avec regex mail

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,17 @@ require('../models/connection');
 const User = require('../models/users');
 const bcrypt = require('bcrypt');
 const uid2 = require('uid2');
-const { checkbody, checkUsername } = require('../modules/checkBody');
+const { checkbody, checkUsername, checkEmail } = require('../modules/checkBody');
 
 
 // inscription d'un nouvel utilisateur
 router.post('/signup', async (req, res) => {
    if (!checkbody(req.body, ['username', 'password', 'email'])) {
     res.status(400).send({result: false, answer : 'Missing parameters'});
+    return;
+  }
+  if (!checkEmail(req.body.email)) {
+    res.status(400).send({result: false, answer : 'Invalid email'});
     return;
   }
     const userExists = await User.findOne({username: req.body.username});
